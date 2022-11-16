31e254dce2f645914f3105ccb1f426576e35058d
yaoyu <yaoyuli@pqdusa.com>
2022-11-16 09:46:03 +0800

    1. first push

--- src/components/PatchMeta.vue
+++ src/components/PatchMeta.vue
@@ -3,67 +3,70 @@
 </template>
 
 <script lang="ts">
-import { onMounted, defineComponent } from 'vue'
+import { onMounted, defineComponent } from "vue";
 
 const defaultMetas = {
-  siteName: 'vue3-md-blog',
-  title: 'Minimal Vue3 + Markdown blog engine',
-  description: 'Simply fork and deploy!',
-  previewUrl: 'https://github.com/yeikiu/vue3-md-blog/blob/master/src/assets/logo.png?raw=true',
-  shareUrl: 'https://yeikiu.github.io/vue3-md-blog/#/'
-}
+  siteName: "whatsoever",
+  title: "My blog",
+  description: "Some description",
+  previewUrl:
+    "https://github.com/yeikiu/vue3-md-blog/blob/master/src/assets/logo.png?raw=true",
+  shareUrl: "https://yeikiu.github.io/vue3-md-blog/#/",
+};
 
 const patchMeta = ({
   siteName = defaultMetas.siteName,
   title = defaultMetas.title,
   description = defaultMetas.description,
   previewUrl = defaultMetas.previewUrl,
-  shareUrl = defaultMetas.shareUrl
+  shareUrl = defaultMetas.shareUrl,
 }) => {
   // Patch title
-  const [titleEl] = document.getElementsByTagName('title')
-  titleEl.textContent = `${siteName} | ${title} - ${description}`
+  const [titleEl] = document.getElementsByTagName("title");
+  titleEl.textContent = `${siteName} | ${title} - ${description}`;
 
   // Patch META
-  Array.from(document.getElementsByClassName('APP_META')).forEach(el => { el.remove() })
+  Array.from(document.getElementsByClassName("APP_META")).forEach((el) => {
+    el.remove();
+  });
   const APP_META = `<meta class="APP_META" property="og:title" content="${title}">
       <meta class="APP_META" property="og:description" content="${description}">
       <meta class="APP_META" property="og:image" content="${previewUrl}">
       <meta class="APP_META" property="og:url" content="${shareUrl}">
       <meta class="APP_META" name="twitter:card" content="summary_large_image">
-      <meta class="APP_META" property="og:site_name" content="${siteName}">`
-  const [headEl] = document.getElementsByTagName('head')
-  if (!headEl) return
-  headEl.innerHTML = `${headEl.innerHTML}${APP_META}`
-}
+      <meta class="APP_META" property="og:site_name" content="${siteName}">`;
+  const [headEl] = document.getElementsByTagName("head");
+  if (!headEl) return;
+  headEl.innerHTML = `${headEl.innerHTML}${APP_META}`;
+};
 
 export default defineComponent({
   props: {
     siteName: {
       type: String,
-      default: defaultMetas.siteName
+      default: defaultMetas.siteName,
     },
     title: {
       type: String,
-      default: defaultMetas.title
+      default: defaultMetas.title,
     },
     description: {
       type: String,
-      default: defaultMetas.description
+      default: defaultMetas.description,
     },
     previewUrl: {
       type: String,
-      default: defaultMetas.previewUrl
+      default: defaultMetas.previewUrl,
     },
     shareUrl: {
       type: String,
-      default: defaultMetas.shareUrl
-    }
+      default: defaultMetas.shareUrl,
+    },
   },
-  setup (props) {
+  setup(props) {
     onMounted(() => {
-      patchMeta(props)
-    })
-  }
-})
+      patchMeta(props);
+    });
+  },
+});
 </script>
